d1f67f2c99b8b4af1008e23d2f06b25d8a6af398
Andrejus Timko <timcostudios@gmail.com>
2021-05-17 15:49:12 +0300

    Sukurtas laikas ir data

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const h1Text = document.querySelector("h1");
 console.log(h1Text);
 
 const myDate = new Date();
-myDate.toLocaleDateString();
-myDate.toLocaleTimeString();
-console.log(myDate);
+const dataString = myDate.toLocaleDateString();
+const timeString = myDate.toLocaleTimeString();
+// console.log(dataString);
+
 const h4Text = document.createElement("h4");
 h4Text.className = "Date and Time";
-h4Text.textContent = myDate;
+h4Text.textContent = dataString + " " + timeString;
+document.body.append(h4Text);
 console.log(h4Text);
